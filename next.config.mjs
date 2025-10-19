4bf9dff62dec3d8e154d0bec6ca08f89b07f2d6e
saumitrakulkarni4823 <saumitrakulkarni4823@gmail.com>
2025-10-19 00:09:25 +0000

    feat: Configure for GitHub Pages

---
+++ next.config.mjs
@@ -0,0 +1,11 @@
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  basePath: '/sth-test',
+  images: {
+    unoptimized: true,
+  },
+};
+
+export default nextConfig;
